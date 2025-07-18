774e2d6ad249a815b24b53192ada47cd94f6df6f
GiorgiZabakhidze <110590273+GiorgiZabakhidze@users.noreply.github.com>
2025-07-18 13:38:46 +0400

    Time: 243 ms (15.64%) | Memory: 55.7 MB (6.74%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
+
+bool solve(int i,vector<int>&nums,vector<int>&dp){
+
+if(i>=nums.size()-1){
+return 1;
+}
+if(dp[i]!=-1){
+    return dp[i];
+}
+if(i!=nums.size()-1 and nums[i]==0){
+    return dp[i]=0;
+}
+
+for(int j=nums[i];j>=1;j--){
+ if(solve(i+j,nums,dp)){
+     return dp[i]= 1;
+ }
+}
+
+return dp[i]=0;
+}
+
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int goal = n - 1;
-        for(int i = n - 2; i >= 0; --i) {
-            if(i + nums[i] >= goal)
-                goal = i;
-        }
-        return goal == 0;
+      int n=nums.size();
+      vector<int>dp(n+1,-1);
+      return solve(0,nums,dp); 
     }
 };
