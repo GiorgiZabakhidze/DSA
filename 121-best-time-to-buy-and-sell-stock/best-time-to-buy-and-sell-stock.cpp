60e2030396b5fe07198b92cea53403624d9a90e7
GiorgiZabakhidze <110590273+GiorgiZabakhidze@users.noreply.github.com>
2025-07-21 18:05:49 +0400

    Time: 187 ms (6.69%) | Memory: 168.9 MB (5.27%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,24 @@
 class Solution {
+private:
+    int find(vector<int>& prices, int idx, bool buy, int k, vector<vector<int>>& memo) {
+        if(idx >= prices.size() || k == 0) 
+            return 0;
+        if(memo[idx][buy] != -1)
+            return memo[idx][buy];
+        
+        if(buy) {
+            return memo[idx][buy] = max(-prices[idx] + find(prices, idx + 1, !buy, k, memo), 
+                                        find(prices, idx + 1, buy, k, memo));
+        }else {
+            return memo[idx][buy] = max(prices[idx] + find(prices, idx + 1, !buy, k - 1, memo), 
+                                        find(prices, idx + 1, buy, k, memo));
+        }
+    }
 public:
     int maxProfit(vector<int>& prices) {
-        int minSoFar = INT_MAX;
-        int ans = 0;
-        for(auto &it : prices) {
-            ans = max(ans, it - minSoFar);
-            minSoFar = min(minSoFar, it);
-        }
-        return ans;
+        int n = prices.size();
+        vector<vector<int>> memo(n, vector<int> (2, -1));
+
+        return find(prices, 0, true, 1, memo);
     }
-// private:
-//     int dp(vector<int>& prices, )
 };
